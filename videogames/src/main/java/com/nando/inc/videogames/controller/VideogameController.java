20000ef0ad04931cc21379d53d57ce5bbc27f5b0
Fernando Peñaranda <fpenarandat@gmail.com>
2020-07-19 00:15:00 -0500

    [Videogames] - Added a new form to manage the seraching by name. - Connected Logic between view and controller to perform searching. - Added th:action to form tag which allows going through one specific task. - Added th:placeholder to form tag which allows to keep updated the String given during the searching. - Modified method on the controller to handle the parameters as a pseudonym.

--- videogames/src/main/java/com/nando/inc/videogames/controller/VideogameController.java
+++ videogames/src/main/java/com/nando/inc/videogames/controller/VideogameController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Centralized Videogames controller.
@@ -68,7 +69,7 @@ public class VideogameController {
      * @return String which contains what is passed to the model. 
      */
     @RequestMapping("/videogamesByName")
-    public String listVideogameByName(String nameToMatch, Model model)
+    public String listVideogameByName(@RequestParam("q") String nameToMatch, Model model)
     {
         final List<Videogame> videogamesByName = videogameService.findByVideogameNameContaining(nameToMatch);
         model.addAttribute("highlightedVideogames", videogamesByName);
